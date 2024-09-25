082a5f283297e04ebdea1fac5f1dde414f8171ad
jhairyn <jhairynsobrevega22@gmail.com>
2024-09-25 22:21:45 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,21 @@
 <?php
-// db.php
-$servername = "localhost";
-$username = "root";  // Replace with your MySQL username
-$password = "";  // Replace with your MySQL password
-$dbname = "metro_events";
+//echo "Hello World";
+ 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$host = 'jhai-server.mysql.database.azure.com';
+$db = 'jhai-database';
+$user = 'fcjxiakiqb';
+$pass = '9B$8AyRP5a1zMDUY';
+
+
+// Create a MySQLi connection
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); // Optional: Adjust if you have specific SSL certs
+mysqli_real_connect($conn, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+//echo "Connected successfully";
 ?>

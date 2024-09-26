4057a36c9c0a729d1c7a6421d63dd77fd1194eeb
jhairyn <jhairynsobrevega22@gmail.com>
2024-09-26 20:53:36 +0800

    Update dashb.php

--- dashb.php
+++ dashb.php
@@ -30,7 +30,7 @@ $_SESSION['role'] = $role;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Role Based Display</title>
+     <title>Jhairyn D. Sobrevega</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
